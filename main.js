8bf4fac236837ec9191c3e1cb97244dcf69ef514
Piotr Konieczny <piotrekprogramista@gmail.com>
2022-09-28 20:45:22 +0200

    check the length of the text & compare the passwords

--- main.js
+++ main.js
@@ -29,15 +29,32 @@ const checkForm = (input) => {
   });
 };
 
+const checkLength = (input, min) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${input.previousElementSibling.innerText.slice(0, -1)} musi składać się z min. ${min} znaków.`,
+    );
+  }
+};
+
+const checkPassword = (pass1, pass2) => {
+  if (pass1.value !== pass2.value) {
+    showError(pass2, 'Hasła do siebie nie pasują!');
+  }
+};
+
 sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   checkForm([username, pass, pass2, email]);
+  checkLength(username, 3);
+  checkLength(pass, 8);
+  checkPassword(pass, pass2);
 });
 
 clearBtn.addEventListener('click', (e) => {
   e.preventDefault();
-
   [username, pass, pass2, email].forEach((el) => {
     el.value = '';
   });
